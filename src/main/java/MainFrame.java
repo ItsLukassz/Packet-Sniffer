c8536b78375cccb0731429033d0caad67791fdc6
ItsLukass <itslukass123@gmail.com>
2022-05-19 21:29:11 +0300

    Changed the design to make it a bit cleaner and added a check for the field to only accept numbers

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -8,27 +8,48 @@ public class MainFrame extends JFrame {
     private JButton stopPacketSnifferButton;
     private JLabel RunningTime;
     private JPanel mainPanel;
+    private JLabel status;
 
     public MainFrame() {
         setContentPane(mainPanel);
         setTitle("Packet Sniffer");
-        setSize(500, 500);
+        setSize(500, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+        // Start button
         startPacketSnifferButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String timeAsString = AmountInSeconds.getText();
                 System.out.println(timeAsString);
+                int time;
+
+                // check if timeAsString string contains anything except numbers
+                // if it does we give an error message
+                // if it doesn't we convert it to an int and start the sniffer
+                if (timeAsString.matches("[0-9]+")) {
+                    time = Integer.parseInt(timeAsString);
+                    RunningTime.setText("Running for: " + time + " seconds");
+                    status.setText("Status: Running");
+                }else {
+                    JOptionPane.showMessageDialog(null, "Please enter a number");
+                }
+
+
             }
         });
+
+        // Stop button
         stopPacketSnifferButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Stop");
+                status.setText("Status: Stopped");
                 // Create a new error message
                 JOptionPane.showMessageDialog(null, "Packed sniffer stopped", "Packet sniffer", JOptionPane.INFORMATION_MESSAGE);
+
+
 
             }
         });
@@ -38,4 +59,5 @@ public class MainFrame extends JFrame {
         MainFrame mainFrame = new MainFrame();
 
     }
+
 }
